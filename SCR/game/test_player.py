e9378a0f70b653b578bbe4abf4f3d1e27beecffc
Ludde <94676134+Ludde94@users.noreply.github.com>
2022-03-01 12:39:43 +0100

    testplayer

--- SCR/game/test_player.py
+++ SCR/game/test_player.py
@@ -4,7 +4,7 @@ import Player
 class TestPlayer(unittest.TestCase):
     def test_player(self):
         play= Player.player()
-        self.assertIsInstance(play, Player)
+        self.assertIsInstance(play, Player.player)
         
 if __name__ == '__main__':
     unittest.main()
