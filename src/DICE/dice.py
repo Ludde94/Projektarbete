2fca1d6c0a5946a13b46ab655d94fee94e555786
Ludde <94676134+Ludde94@users.noreply.github.com>
2022-03-01 10:16:17 +0100

    comments

--- src/DICE/dice.py
+++ src/DICE/dice.py
@@ -4,11 +4,12 @@ class RolledOneException(Exception):
     pass
 
 class Dice:
+    """creating dice with random"""
     def __init__(self):
         self.value = random.randint(1, 6)
 
     def roll(self):
-    
+        """creating roll with random value"""
         self.value = random.randint(1, 6)
         if self.value == 1:
             raise RolledOneException
