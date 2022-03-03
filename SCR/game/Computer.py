17c33c298d00ba559fd53c1ca6d52cba40ea40bc
Ludde <94676134+Ludde94@users.noreply.github.com>
2022-03-03 15:21:05 +0100

    added rolls_made

--- SCR/game/Computer.py
+++ SCR/game/Computer.py
@@ -3,20 +3,21 @@ import dice
 class computer: 
     create = dice.Dice
     turnvalue = create.get_value
+    rollsmade = create.get_rolls_made
     
     def __init__(self):
         self.computer = computer
         
-    def computer_roll_hard(self,turnvalue): #beslut utifrån turnvalue
+    def computer_roll_hard(self,turnvalue,rollsmade): #beslut utifrån turnvalue
         """computer hard setting"""
-        while turnvalue < 20: #lägga till rollsmade<6
+        while turnvalue < 20 and rollsmade < 6: 
             print("computer will roll")
             return True
         return False
         
-    def computer_roll_easy(self,turnvalue):
+    def computer_roll_easy(self,turnvalue,rollsmade):
         """computer easy setting"""
-        while turnvalue < 30:
+        while turnvalue < 30 and rollsmade < 10:
             print("computer will roll")
             return True
         return False
--- SCR/game/dice.py
+++ SCR/game/dice.py
@@ -5,17 +5,23 @@ class Dice():
     def __init__(self):
         """creating dice with random"""
         self.sum_rolls = 0
+        self.sum_rolls_made = 0
         self.value = random.randint(1, 6)
 
     def roll(self):
         """creating roll with random value"""
         self.value = random.randint(1, 6)
         self.sum_rolls += self.value #funkar inte att addera här, hjälp
+        self.sum_rolls_made += 1
         return self.value
     
     def get_value(self):#funkar inte att addera, får ingen output
         """get value all rolls"""
         return self.sum_rolls
+    
+    def get_rolls_made(self):
+        """get how many rolls were made"""
+        return self.sum_rolls_made
         
     def __str__(self):
         """Print rolled value"""
