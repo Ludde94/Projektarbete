6599ff21003cc4f4bd9ae02303dd499a3208319a
Ludde <94676134+Ludde94@users.noreply.github.com>
2022-03-07 11:34:22 +0100

    big letters

--- SCR/game/HighScore.py
+++ SCR/game/HighScore.py
@@ -1,4 +1,4 @@
-class HighScore():
+class Highscore():
 
     def __init__(self):
         """object of class"""
--- SCR/game/game.py
+++ SCR/game/game.py
@@ -1,15 +1,15 @@
 import dice
 import menu
-import Player
-import Computer
-import HighScore
+import player
+import computer
+import highscore
 
 class game():
     die = dice.Dice()
     info = menu.Menu()
-    player = Player.player
-    comp = Computer.Computer
-    high = HighScore.HighScore
+    player = player.Player()
+    comp = computer.Computer
+    high = highscore.Highscore
     
     info.welcome
     info.game_menu
