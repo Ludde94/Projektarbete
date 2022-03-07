073a1f1c2045c5d3d324d2b1c90ba83cb0224893
Ludde <94676134+Ludde94@users.noreply.github.com>
2022-03-07 14:04:55 +0100

    added sort and save highscore

--- SCR/game/HighScore.py
+++ SCR/game/HighScore.py
@@ -1,9 +1,18 @@
-class Highscore():
+import dice
+import player
 
+class Highscore():
+    score = dice.Dice
+    rollsmade = score.get_rolls_made
+    player= player.Player
+    name = player.player
+    
+    
     def __init__(self):
         """object of class"""
-        self.add_highscore
+        self.save_to_highscore
         self.highscores
+        
 
     def highscores(self):
         with open('Highscores.txt', 'r') as Highscores_file:
@@ -13,7 +22,21 @@ class Highscore():
                 print(f'{name} scored {count} points.')
                 name = Highscores_file.readline().rstrip('\n')
 
-    def add_highscore():
-        pass
 
-    def 
+    def save_to_highscore(self, rollsmade, name):
+        with open('Highscores.txt', 'w') as Highscores_file:
+            Highscores_file.write(f'{name} with {rollsmade} rolls')
+            
+            
+    def sort_highscore(self):
+        scores = []
+        with open('Highscores.txt', 'r') as Highscores_file:
+            for line in Highscores_file:
+                name = Highscores_file.readline().rstrip('\n')
+                score =  Highscores_file.readline().rstrip('\n')
+                scores.append(int(score))
+        scores.sort(reverse=True)
+        print(f'{name} with {scores} rolls'[0:5])
+            
+            
+                
--- SCR/game/main.py
+++ SCR/game/main.py
@@ -1,12 +1,13 @@
 import sys
 import dice
 import menu
-import computer
-import player
+import highscore
 
 def main():
    welcome_menu = menu.Menu()
    welcome_menu.welcome()
+   
+   
 
 if __name__ == "__main__":
     # Call the main function.
