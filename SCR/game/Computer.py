6614480233a154e63a05427701a0b47380a894fa
Fredrik Johnsson <fredrik.johnsson0029@stud.hkr.se>
2022-03-08 16:12:35 +0100

    Start game

--- SCR/game/Computer.py
+++ SCR/game/Computer.py
@@ -1,8 +1,8 @@
 from dice import Dice
 from random import randint, choices
 
-class Computer(): 
-    
+class Computer():
+
     def __init__(self):
         """computer object"""
         self.name = "Ultimate computer"
@@ -11,13 +11,12 @@ class Computer():
         self.current_rolls = 0
         self.current_sum = 0
         self.box = 0
-        
-        
-        
+
+
     def set_difficulty(self, difficulty):
         self.difficulty = difficulty
-        
-        
+
+
     def play(self):
         self.running = True
         weights_easy = [10, 15, 20, 25, 30, 50]
@@ -26,8 +25,8 @@ class Computer():
         hold_easy = [1, 3]
         threshold_easy = 40
         threshold_hard = 20
-        
-    
+
+
         while self.running:
             if self.difficulty == "Easy":
                 result = choices(self.dice.sides, weights_easy)[0]#kontroll räkna ludde
@@ -35,21 +34,21 @@ class Computer():
                 result = choices(self.dice.sides, weights_hard)[0]#kontroll räkna ludde
             self.current_rolls += 1
             if result == 1:
-                print("Opps i got a 1, your turn") 
+                print("Opps i got a 1, your turn")
                 print(f'Computer still has a score of {self.current_sum}')
                 self.box = 0
                 self.running = False
-            else: 
+            else:
                 print(f'Bot rolled: {result}')
                 self.box += result
-                
+
                 if self.difficulty == "Easy":
                     threshold = threshold_easy
                 elif self.difficulty == "Hard":
-                    threshold = threshold_hard    
-                
+                    threshold = threshold_hard 
+
                 if self.current_sum >= threshold:
-                    if self.difficulty == "Easy": 
+                    if self.difficulty == "Easy":
                         hold = choices([True, False], weights = hold_easy)[0]
                     elif self.difficulty == "Hard":
                         hold = choices([True, False], weights = hold_hard)[0]
@@ -57,7 +56,3 @@ class Computer():
                         print(f'Computer chooses to hold, with a value of {self.current_sum}')
                         self.current_sum += self.box
                         self.running = False
-            
-                
-        
-       
--- SCR/game/Player.py
+++ SCR/game/Player.py
@@ -1,19 +1,43 @@
+from dice import Dice
+from random import randint
+
 class Player():
     """"creating player object"""
     def __init__(self, name):
         self.name = name
-        
+        self.dice = Dice()
+        self.current_value = 0
+        self.current_rolls = 0
+        self.current_sum = 0
+        self.box = 0
 
-    """"creating player with user input"""
-    def player(self):
-        name = input('Name of player: ')
-        return name
-    
     def changename(self, newname):
         self.name = newname
-    
-    def keep_rolling(self):
+
+    def input_number(self, prompt='Please enter a number: ', minimum=0, maximum=None):
+        """Read a positive number with the given prompt."""
+
+        while True:
+            try:
+                number = int(input(prompt))
+                if (number < minimum or
+                    (maximum is not None and number > maximum)):
+                        print('Number is not within range: {} to {}'.format(minimum, maximum))
+                else:
+                    break
+
+            except ValueError:
+                print('You need to enter a number')
+                continue
+
+        return number
+
+    def play(self):
         """Decision for keep rolling"""
-        print("Press 1 - Roll again, 2 - Hold?")
-        decision = int(input(" "))
-        return decision
+        self.running = True
+        human_decision = self.input_number("  1 - Roll again, 0 - Hold? ", 0, 1)
+        while self.running:
+            if human_decision == 1:
+                result = randint(1, 6) # Här ska vi fortsätta!
+            else:
+                pass
