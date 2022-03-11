24cd4af1a1ec3591b98a70ec333e4d2a0a31054e
david <david0000@hotmail.se>
2022-03-11 11:09:17 +0100

    fixat unit test computer,player,dice 10 testcase och 20 assertions

--- SCR/game/test_computer.py
+++ SCR/game/test_computer.py
@@ -1,13 +1,70 @@
 import unittest
-import computer
+from computer import Computer
 
-class TestPlayer(unittest.TestCase):
-    def setUp(self):
-        self.running = computer.Computer()
+class TestComputer(unittest.TestCase):
+    
+    def test_is_instance(self):
+        comp = Computer(100)
+        self.assertIsInstance(comp, Computer)
 
-    def test_play(self):
-        self.assertTrue(self.running)
+    def test_set_difficulty(self):
+        comp = Computer(100)
+        self.assertEqual(comp.difficulty, None)
+        comp.set_difficulty("Easy")
+        self.assertNotEqual(comp.difficulty, None)
+
+    def test_set_difficulty_2(self):
+        comp = Computer(100)
+        comp.set_difficulty("Easy")
+        self.assertEqual(comp.difficulty, "Easy")
+
+    def test_set_difficulty_3(self):
+        comp = Computer(100)
+        comp.set_difficulty("Easy")
+        self.assertNotEqual(comp.difficulty, "Hard")
+
+    def test_set_difficulty_4(self):
+        comp = Computer(100)
+        comp.set_difficulty("Hard")
+        self.assertEqual(comp.difficulty, "Hard")
+
+    def test_set_difficulty_5(self):
+        comp = Computer(100)
+        comp.set_difficulty("Hard")
+        self.assertNotEqual(comp.difficulty, "Easy")
+
+    def test_set_treshhold_difficulty(self):
+        comp = Computer(100)
+        comp.set_difficulty("Easy")
+        threshhold = comp.set_treshhold_difficulty(20, 15)
+        self.assertEqual(threshhold, 20)
+
+    def test_set_treshhold_difficulty_2(self):
+        comp = Computer(100)
+        comp.set_difficulty("Hard")
+        threshhold = comp.set_treshhold_difficulty(20, 15)
+        self.assertEqual(threshhold, 15)
+
+    def test_set_treshhold_difficulty_3(self):
+        comp = Computer(100)
+        comp.set_difficulty("Easy")
+        threshhold = comp.set_treshhold_difficulty(20, 15)
+        self.assertNotEqual(threshhold, 15)
+
+    def test_set_treshhold_difficulty_4(self):
+        comp = Computer(100)
+        comp.set_difficulty("Hard")
+        threshhold = comp.set_treshhold_difficulty(20, 15)
+        self.assertNotEqual(threshhold, 20)
+
+    def test_computer_got_1(self):
+        comp = Computer(100)
+        result = comp.computer_got_1()
+        self.assertEqual(result, False)
+
+if __name__ == '__main__':
+    unittest.main()
+    
         
         
-    if __name__ == '__main__':
-        unittest.main()
+    
--- SCR/game/test_dice.py
+++ SCR/game/test_dice.py
@@ -1,30 +1,87 @@
 import unittest
 
-import dice
+from dice import Dice
+
+# class TestDice(unittest.TestCase):
+#     """Test dice class"""
+#     def setUp(self):
+#         self.die = dice.Dice()
+#         self.die1 = dice.Dice()
+
+
+#     def test_dice(self):
+#         """Test dice object"""
+#         self.assertIsInstance(self.die, dice.Dice)
+
+#     def test_roll(self):
+#         """Test dice roll"""
+
+#         res = self.die1.value
+#         exp = 1 <= res <= self.die1.value
+#         self.assertTrue(exp)
+
+#     def test_roll_betweem_one_to_six(self):
+#         """Testing if rolled dice gives value between 1-6"""
+#         self.die1.roll()
+#         self.assertIn(self.die1.value, [1,2,3,4,5,6])
+
 
 class TestDice(unittest.TestCase):
-    """Test dice class"""
-    def setUp(self):
-        self.die = dice.Dice()
-        self.die1 = dice.Dice()
+    
+    def test_is_instance(self):
+        dice = Dice()
+        self.assertIsInstance(dice, Dice)
 
+    def test_get_rolls_made(self):
+        dice = Dice()
+        rolls = dice.get_rolls_made()
+        self.assertEqual(rolls, 0)
 
-    def test_dice(self):
-        """Test dice object"""
-        self.assertIsInstance(self.die, dice.Dice)
+    def test_get_rolls_made_2(self):
+        dice = Dice()
+        dice.sum_rolls_made = 4
+        rolls = dice.get_rolls_made()
+        self.assertEqual(rolls, 4)
 
-    def test_roll(self):
-        """Test dice roll"""
+    def test_get_rolls_made_3(self):
+        dice = Dice()
+        dice.sum_rolls_made = 4
+        rolls = dice.get_rolls_made()
+        self.assertNotEqual(rolls, 0)
 
-        res = self.die1.value
-        exp = 1 <= res <= self.die1.value
-        self.assertTrue(exp)
+    def test_get_sum_rolls(self):
+        dice = Dice()
+        _sum = dice.get_sum_rolls()
+        self.assertEqual(_sum, 0)
 
-    def test_roll_betweem_one_to_six(self):
-        """Testing if rolled dice gives value between 1-6"""
-        self.die1.roll()
-        self.assertIn(self.die1.value, [1,2,3,4,5,6])
+    def test_get_sum_rolls_2(self):
+        dice = Dice()
+        dice.sum_rolls = 10
+        _sum = dice.get_sum_rolls()
+        self.assertEqual(_sum, 10)
 
+    def test_get_sum_rolls_3(self):
+        dice = Dice()
+        dice.sum_rolls = 10
+        _sum = dice.get_sum_rolls()
+        self.assertNotEqual(_sum, 0)
+
+    def test_str(self):
+        dice = Dice()
+        TEXT = "Rolled " + str(dice.value) + "."
+        self.assertEqual(str(dice), TEXT)
+
+    def test_str_2(self):
+        dice = Dice()
+        dice.value = 5
+        TEXT = "Rolled " + str(dice.value) + "."
+        self.assertEqual(str(dice), TEXT)
+
+    def test_str_3(self):
+        dice = Dice()
+        TEXT = "Rolled " + str(dice.value) + "."
+        dice.value = 5
+        self.assertNotEqual(str(dice), TEXT)
 
 if __name__ == '__main__':
     unittest.main()
